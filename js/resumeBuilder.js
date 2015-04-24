f545c6810a25ff765480872dc6add904f8919d71
coolzai <coolzai@gmail.com>
2015-04-24 00:27:33 -0400

    update

---
+++ js/resumeBuilder.js
@@ -0,0 +1,73 @@
+// var formattedName = HTMLheaderName.replace("%data%", "Kai Liu");
+//
+// var role = "Mechancial Engineering"
+// var formattedRole = HTMLheaderRole.replace("%data%", role)
+//
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+var bio = {
+    "name" : "Kai Liu",
+    "role" : "Mechanical Engineering",
+    "contacts" : {
+        "mobile": "317-970-8072",
+        "email": "liu915@purdue.edu",
+        "github": "coolzai",
+        "twitter": "@coolzai",
+        "location": "Indianapolis, IN"
+    },
+    "welcomeMessage" : "Test test test",
+    "skills": [
+        "a", "b", "c"
+    ],
+    "bioPic" : "images/fry.jpg"
+};
+
+var work = {
+    "jobs" : [
+    {   "employer" : "employer",
+        "title" : "work position",
+        "location" : "location",
+        "dates" : 0.3,
+        "description" : "some descpt"
+    },
+    {   "employer" : "employer",
+        "title" : "work position",
+        "location" : "location",
+        "dates" : 0.3,
+        "description" : "some descpt"
+    }
+    ]
+};
+
+var projects = {
+    "projects" : [
+    {
+        "title" : "title",
+        "dates" : 2015,
+        "description" : "test",
+        "images" : ["images/fry.jps"]
+    }
+    ]
+};
+
+var education = {
+    "schools" : [
+    {
+        "name" : "Purdue University",
+        "location" : "West Lafayette, IN",
+        "degree" : "Doctor of Philosophy",
+        "majors" : ["Mechancial Engineering"],
+        "dates" : 2017,
+        "url" : "http://www.purdue.edu"
+    },
+    {
+        "name" : "Purdue University",
+        "location" : "West Lafayette, IN",
+        "degree" : "Master of Science",
+        "majors" : ["Mechancial Engineering"],
+        "dates" : 2013,
+        "url" : "http://www.purdue.edu"
+    }
+    ]
+};
